e8811d0bc9b05f83333f2e09af106f2cc3b7f739
sajad-ahmadnzhad <ahmadnzhad22@gmail.com>
2026-02-03 03:00:32 +0330

    feat: add get one product api method

--- src/api/product.api.ts
+++ src/api/product.api.ts
@@ -12,3 +12,13 @@ export async function getProducts(filter: Record<string, string> = {}): Promise<
     return handleApiErrorResponse(error);
   }
 }
+
+export async function getOneProduct(productId: number): Promise<TApiResponse> {
+  try {
+    const response = await http.get(`/product/${productId}`);
+
+    return handleApiSuccessResponse(response);
+  } catch (error) {
+    return handleApiErrorResponse(error);
+  }
+}
