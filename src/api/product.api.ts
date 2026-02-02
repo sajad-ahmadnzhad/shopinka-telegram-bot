669b24216ad5de6bb5589c5899a237cfcb3d2645
sajad-ahmadnzhad <ahmadnzhad22@gmail.com>
2026-02-03 02:52:25 +0330

    feat: add get products api method

---
+++ src/api/product.api.ts
@@ -0,0 +1,14 @@
+import { handleApiErrorResponse, handleApiSuccessResponse, normalizeQuery, TApiResponse } from '../common/utils/functions.utils';
+import { http } from './http.client';
+
+export async function getProducts(filter: Record<string, string> = {}): Promise<TApiResponse> {
+  try {
+    const query = normalizeQuery(filter);
+
+    const response = await http.get(`/product${query}`);
+
+    return handleApiSuccessResponse(response);
+  } catch (error) {
+    return handleApiErrorResponse(error);
+  }
+}
