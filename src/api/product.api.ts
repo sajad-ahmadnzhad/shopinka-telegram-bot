ed458abe2f26ce85fe3ac07bd5473b60ed3bee2e
sajad-ahmadnzhad <ahmadnzhad22@gmail.com>
2026-02-06 22:23:00 +0330

    feat: add getFavoriteProduct and toggleFavoriteProduct methods

--- src/api/product.api.ts
+++ src/api/product.api.ts
@@ -22,3 +22,23 @@ export async function getOneProduct(productId: number): Promise<TApiResponse> {
     return handleApiErrorResponse(error);
   }
 }
+
+export async function getFavoriteProduct(accessToken: string, productId: number): Promise<TApiResponse> {
+  try {
+    const response = await http.get(`/product/${productId}/favorite`, { headers: { Authorization: `bearer ${accessToken}` } });
+
+    return handleApiSuccessResponse(response);
+  } catch (error) {
+    return handleApiErrorResponse(error);
+  }
+}
+
+export async function toggleFavoriteProduct(accessToken: string, productId: number): Promise<TApiResponse> {
+  try {
+    const response = await http.patch(`/product/favorite-toggle/${productId}`, { headers: { Authorization: `bearer ${accessToken}` } });
+
+    return handleApiSuccessResponse(response);
+  } catch (error) {
+    return handleApiErrorResponse(error);
+  }
+}
