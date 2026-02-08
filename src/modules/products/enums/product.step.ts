3fbd230a50f17c27274cacae43ae3ab7b547cf73
sajad-ahmadnzhad <ahmadnzhad22@gmail.com>
2026-02-08 12:17:02 +0330

    feat: add productCommentStep enum

---
+++ src/modules/products/enums/product.step.ts
@@ -0,0 +1,6 @@
+export enum AddProductCommentStep {
+  Title = 'enter_product_comment_title',
+  Content = 'enter_product_comment_content',
+  IsRecommended = 'enter_product_comment_isRecommended',
+  Rate = 'enter_product_comment_rate',
+}
